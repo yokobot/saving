35e7ee3065bd3a945b0e6803be07a12272d4dc75
Masahiro Takeda <masahiro.takeda@miidas.jp>
2022-09-09 00:31:02 +0900

    create mock

---
+++ next-app/styles/dashboard.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+
+`;
+
+export const SubContainer = styled.div`
+    display: block;
+    position: relative;
+    width: 50%;
+    min-width: 500px;
+    height: auto;
+    padding: 0 50px;
+    background-color: grey;
+`;
+
+export const MainBlock = styled.div`
+    display: block;
+    position: relative;
+    width: 100%;
+    height: auto;
+    min-height: 50px;
+    margin: 20px 0;
+    padding: 0 50px;
+    background-color: blue;
+`;
